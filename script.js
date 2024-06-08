845bdf4cd5b0a0fc5eab610f517ed0c73ba540c5
Ajay <mailtoajay7007@gmail.com>
2024-06-08 16:39:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+//theme switch code
+
+const theme = document.getElementsByClassName("theme");
+
+
+
+theme[0].addEventListener("click",()=>{
+    document.body.classList.toggle("dark-mode");
+   
+    
+
+})
